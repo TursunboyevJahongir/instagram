a55c64570c6baee2ca16214c5899b3ff8da6398f
Tursunboyev Jahongir <jah6332@gmail.com>
2020-06-23 20:39:53 +0500

    add Username to user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,13 @@
 <div class="row m-5">
     <div class="col-3"> <img src="https://instagram.fhrk1-1.fna.fbcdn.net/v/t51.2885-19/s320x320/58454178_375182363092278_1762065042477416448_n.jpg?_nc_ht=instagram.fhrk1-1.fna.fbcdn.net&_nc_ohc=qoLzgohynk8AX-CCCQQ&oh=4b74fc9c6689692025240c55d69b4f67&oe=5F1AC997" class="rounded-circle w-50" style="border: 1px solid rgb(66, 66, 66)"></div>
     <div class="col-9">
-        <div><h1>Username</h1></div>
+    <div><h1>{{Auth::user()->username}}</h1></div>
         <div class="d-flex">
             <div class='pr-4'><strong>64</strong> публикаций</div>
             <div class='pr-4'><strong>193</strong>подписчиков</div>
             <div class='pr-4'><strong>222</strong>подписок</div>
         </div>
-        <div class="pt-4 "><b>Name</b></div>
+    <div class="pt-4 "><b>{{Auth::user()->name}}</b></div>
         <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, reprehenderit doloremque blanditiis neque iste quaerat eaque, soluta corrupti reiciendis quos officiis accusamus sequi at tempora quo? Voluptatem quis accusamus reiciendis cum magni iste nihil quia, nisi voluptas eligendi est hic, veniam, alias ut assumenda laudantium mollitia quaerat excepturi perspiciatis autem?</div>
     </div>
 </div>
